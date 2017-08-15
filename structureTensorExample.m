83afb0a47c77987dd4de2535129138bb48ce3e48
Chaowei Shan <cwshan@aliyun.com>
2017-08-15 22:34:41 +0800

    update

--- structureTensorExample.m
+++ structureTensorExample.m
@@ -7,9 +7,17 @@ imshow(img);
 
 maskSize = 11;
 
-DoG = difference_of_gaussian_kernels(maskSize);
-Ix = conv2(img, DoG.Gx,'same');
-Iy = conv2(img, DoG.Gy,'same');
+%DoG = difference_of_gaussian_kernels(maskSize);
+%Ix = conv2(img, DoG.Gx,'same');
+%Iy = conv2(img, DoG.Gy,'same');
+
+Ga = double(fspecial('gaussian', maskSize));
+Gx = conv2(img,[-1,1],'same');
+Gy = conv2(img,[-1;1],'same');
+
+xx = conv2(Gx.^2,Ga,'same');
+xy = conv2(Gx.*Gy,Ga,'same');
+yy = conv2(Gy.^2,Ga,'same');
 
 
 [rows,cols] = size(img);
@@ -18,9 +26,9 @@ result = zeros(rows,cols);
 %MI = [Ix*Ix, Ix*Iy;Ix*Iy, Iy*Iy];
 for r = 1:rows
     for c = 1:cols
-        MI = [Ix(r,c)^2,Ix(r,c)*Iy(r,c);Ix(r,c)*Iy(r,c),Iy(r,c)^2];
+        MI = [xx(r,c),xy(r,c);xy(r,c),yy(r,c)];
         [e1,e2,l1,l2] = eigen_decomposition(MI);
-        if(l2 - l1 > 0.007 && l1 < 1e-17)
+        if(l2 - l1 > 0.007 && l1 < 0.002)
             result(r,c) = 1;
         end
     end
